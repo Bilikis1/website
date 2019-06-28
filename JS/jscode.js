42366c37ef3e8aedd60779ca3926543737ab0b2d
Afolabi Bilikis <bilikisafo@gmail.com>
2019-06-28 16:19:08 +0100

    responsive update

---
+++ JS/jscode.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "80%";
+}
+  // js for close nav onclick closeNav or .container
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+document.querySelectorAll('.home')
+    .addEventListener('click', event => {
+        event.preventDefault();
+        
+    })
