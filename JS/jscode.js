206a8bd58a618eb46f9c875ccd6208dc334fa345
Afolabi Bilikis <bilikisafo@gmail.com>
2019-06-28 16:22:34 +0100

    responsive update

--- JS/jscode.js
+++ JS/jscode.js
@@ -1,12 +1,14 @@
 function openNav() {
-    document.getElementById("myNav").style.width = "80%";
+  document.getElementById("myNav").style.width = "70%";
+  // var nav = document.getElementById("myNav");
+  // nav.classList.toggle('active-menu');
 }
   // js for close nav onclick closeNav or .container
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
 document.querySelectorAll('.home')
     .addEventListener('click', event => {
         event.preventDefault();
         
-    })
+    }) 
