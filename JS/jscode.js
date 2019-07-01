fb95461221142495f2ce31c6f582d0713e807425
Afolabi Bilikis <bilikisafo@gmail.com>
2019-07-01 11:27:32 +0100

    hmi responsive website updated 1july19

--- JS/jscode.js
+++ JS/jscode.js
@@ -1,5 +1,5 @@
 function openNav() {
-  document.getElementById("myNav").style.width = "70%";
+  document.getElementById("myNav").style.width = "80%";
   // var nav = document.getElementById("myNav");
   // nav.classList.toggle('active-menu');
 }
